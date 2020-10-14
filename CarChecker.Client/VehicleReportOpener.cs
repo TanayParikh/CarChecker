29ee256f40a14b44bba15e9ab3d0a8b649ac0e95
Daniel Roth <daroth@microsoft.com>
2020-10-13 19:42:38 -0700

    Add hybrid projects

---
+++ CarChecker.Client/VehicleReportOpener.cs
@@ -0,0 +1,17 @@
+﻿using CarChecker.Client.Data;
+using CarChecker.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarChecker.Client
+{
+    public class VehicleReportOpener : IVehicleReportOpener
+    {
+        public void OpenReport(Vehicle vehicle)
+        {
+            throw new PlatformNotSupportedException();
+        }
+    }
+}
